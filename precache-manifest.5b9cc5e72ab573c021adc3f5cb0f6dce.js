babfbcaca869426b0bafda6914452995af58693f
Jose Robles <jrrs@computer.org>
2019-10-24 00:24:47 -0400

    Updates

---
+++ precache-manifest.5b9cc5e72ab573c021adc3f5cb0f6dce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adcec609b1f6891bf94f9a6aff9aec2",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "615eddb0616bccfc1e5b",
+    "url": "/clicky_game/static/css/main.adb6d0fa.chunk.css"
+  },
+  {
+    "revision": "d67b6f0d34889443bcbb",
+    "url": "/clicky_game/static/js/2.a88aecb4.chunk.js"
+  },
+  {
+    "revision": "615eddb0616bccfc1e5b",
+    "url": "/clicky_game/static/js/main.60ccb4f2.chunk.js"
+  },
+  {
+    "revision": "a2337716b169f42c1327",
+    "url": "/clicky_game/static/js/runtime-main.699e2fe9.js"
+  }
+]);
